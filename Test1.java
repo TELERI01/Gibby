4bcb6c8b939ec36e5b6f8871394c5f434db0e080
TELERI01 <112411816+TELERI01@users.noreply.github.com>
2023-01-11 13:18:48 -0500

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author tylerlericos
+ *
+ */
+public class Test1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
